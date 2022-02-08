b2e8d61f86fac8a4c5771d2c48dce0ab69c1ab7d
nik-kita <nik.moiseienko@gmail.com>
2022-02-08 15:21:18 +0200

    feat(inspected-service): declare all required api-routes in controller

--- src/components/inspected-service/inspected-service.controller.ts
+++ src/components/inspected-service/inspected-service.controller.ts
@@ -1,6 +1,37 @@
-import { Controller } from '@nestjs/common';
+import {
+    Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseUUIDPipe, Post, Put,
+} from '@nestjs/common';
 
 @Controller('services')
 export class InspectedServiceController {
+    @Get('/:serviceId/status')
+    getService(@Param('serviceId', ParseUUIDPipe) serviceId: string): Promise<unknown> {
+        return Promise.resolve(serviceId);
+    }
 
+    @Get()
+    getAllServices(): Promise<unknown[]> {
+        return Promise.resolve([]);
+    }
+
+    @Post()
+    addService(@Body() data: unknown): Promise<unknown> {
+        return Promise.resolve(data);
+    }
+
+    @Put(':serviceId')
+    updateService(
+        @Param('serviceId', ParseUUIDPipe) serviceId: string,
+        @Body() data: unknown,
+    ): Promise<unknown> {
+        return Promise.resolve({ serviceId, data });
+    }
+
+    @HttpCode(HttpStatus.NO_CONTENT)
+    @Delete(':serviceId')
+    deleteService(@Param('serviceId', ParseUUIDPipe) serviceId: string): Promise<void> {
+        Promise.resolve(serviceId);
+
+        return Promise.resolve();
+    }
 }
