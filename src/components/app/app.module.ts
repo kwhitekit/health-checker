5a9b96f1a09b57f09a75d630bcc00643185f2d31
nik-kita <nik.moiseienko@gmail.com>
2022-02-08 15:35:24 +0200

    feat(app.module): configModule, TypeormModule

--- src/components/app/app.module.ts
+++ src/components/app/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { InspectedServiceModule } from '../inspected-service/inspected-service.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-    imports: [InspectedServiceModule],
+    imports: [
+        ConfigModule.forRoot(),
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: process.env.PG_HOST ?? 'localhost',
+            port: +process.env.PG_PORT ?? 5432,
+            username: process.env.PG_USER ?? 'postgres',
+            password: process.env.PG_PASS ?? 'postgres',
+            database: process.env.PG_DATABASE ?? 'health-checker',
+            synchronize: true,
+        }),
+        InspectedServiceModule,
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
