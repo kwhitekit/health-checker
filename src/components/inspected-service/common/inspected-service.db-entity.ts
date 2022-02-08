bfd94f2be5192cdc16ebe6bd9c1c8dd4bb794abd
nik-kita <nik.moiseienko@gmail.com>
2022-02-08 15:45:27 +0200

    feat(inspected-service): db-entity

--- src/components/inspected-service/common/inspected-service.db-entity.ts
+++ src/components/inspected-service/common/inspected-service.db-entity.ts
@@ -1,11 +1,28 @@
+import { Column } from 'typeorm';
 import { BaseDbEntity } from '../../../general/base.db-entity';
 import { HttpMethodEnum } from '../../../general/http-method.enum';
 import { IInspectedServiceEntity } from './inspected-service.entity.interface';
 
 export class InspectedServiceDbEntity extends BaseDbEntity implements IInspectedServiceEntity {
-    checkUrl: string;
+  @Column({
+      type: 'varchar',
+      length: 500,
+      nullable: false,
+  })
+  checkUrl: string;
 
-    method: HttpMethodEnum;
+  @Column({
+      type: 'varchar',
+      length: 20,
+      enum: HttpMethodEnum,
+      default: HttpMethodEnum.GET,
+  })
+  method: HttpMethodEnum;
 
-    name: string;
+  @Column({
+      type: 'varchar',
+      length: 50,
+      nullable: false,
+  })
+  name: string;
 }
