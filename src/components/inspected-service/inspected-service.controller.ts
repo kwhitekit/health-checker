1f0a519c115c09d3b06ab6ddd7904c1b2c9d0b07
nik-kita <nik.moiseienko@gmail.com>
2022-02-08 16:14:44 +0200

    feat(inspected-service): gET :id, askHealth()

--- src/components/inspected-service/inspected-service.controller.ts
+++ src/components/inspected-service/inspected-service.controller.ts
@@ -1,41 +1,42 @@
 import {
     Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseUUIDPipe, Post, Put,
 } from '@nestjs/common';
+import { HealthReportResDto } from '../../general/health-report.res.dto';
 import { CreateInspectedServiceDto } from './dto/create.inspect-service.dto';
 import { InspectedServiceService } from './inspected-service.service';
 
 @Controller('services')
 export class InspectedServiceController {
-    constructor(private inspectedServiceService: InspectedServiceService) {}
+    constructor(private inspectedServiceService: InspectedServiceService) { }
 
-    @Get('/:serviceId/status')
-    getService(@Param('serviceId', ParseUUIDPipe) serviceId: string): Promise<unknown> {
-        return Promise.resolve(serviceId);
+  @Get('/:serviceId/status')
+    getService(@Param('serviceId', ParseUUIDPipe) serviceId: string): Promise<HealthReportResDto> {
+        return this.inspectedServiceService.askHealth(serviceId);
     }
 
-    @Get()
-    getAllServices(): Promise<unknown[]> {
-        return Promise.resolve([]);
-    }
+  @Get()
+  getAllServices(): Promise<unknown[]> {
+      return Promise.resolve([]);
+  }
 
-    @Post()
-    addService(@Body() data: CreateInspectedServiceDto): Promise<unknown> {
-        return this.inspectedServiceService.createService(data);
-    }
+  @Post()
+  addService(@Body() data: CreateInspectedServiceDto): Promise<unknown> {
+      return this.inspectedServiceService.createService(data);
+  }
 
-    @Put(':serviceId')
-    updateService(
-        @Param('serviceId', ParseUUIDPipe) serviceId: string,
-        @Body() data: unknown,
-    ): Promise<unknown> {
-        return Promise.resolve({ serviceId, data });
-    }
+  @Put(':serviceId')
+  updateService(
+    @Param('serviceId', ParseUUIDPipe) serviceId: string,
+    @Body() data: unknown,
+  ): Promise<unknown> {
+      return Promise.resolve({ serviceId, data });
+  }
 
-    @HttpCode(HttpStatus.NO_CONTENT)
-    @Delete(':serviceId')
-    deleteService(@Param('serviceId', ParseUUIDPipe) serviceId: string): Promise<void> {
-        Promise.resolve(serviceId);
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @Delete(':serviceId')
+  deleteService(@Param('serviceId', ParseUUIDPipe) serviceId: string): Promise<void> {
+      Promise.resolve(serviceId);
 
-        return Promise.resolve();
-    }
+      return Promise.resolve();
+  }
 }
--- src/components/inspected-service/inspected-service.service.ts
+++ src/components/inspected-service/inspected-service.service.ts
@@ -1,15 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import axios from 'axios';
 import { Repository } from 'typeorm';
+import { HealthReportResDto } from '../../general/health-report.res.dto';
+import { HealthStatusEnum } from '../../general/health-status.enum';
 import { InspectedServiceDbEntity } from './common/inspected-service.db-entity';
 import { CreateInspectedServiceDto } from './dto/create.inspect-service.dto';
 
 @Injectable()
 export class InspectedServiceService {
     constructor(
-      @InjectRepository(InspectedServiceDbEntity)
-      private inspectedServiceRepository: Repository<InspectedServiceDbEntity>,
-    ) {}
+    @InjectRepository(InspectedServiceDbEntity)
+    private inspectedServiceRepository: Repository<InspectedServiceDbEntity>,
+    ) { }
+
+    public async askHealth(id: string): Promise<HealthReportResDto> {
+        const service = await this.inspectedServiceRepository.findOne(id);
+        const { status } = await axios({
+            url: service.checkUrl,
+            method: service.method,
+        });
+
+        return {
+            status: status >= 500
+                ? HealthStatusEnum.UNAVAILABLE
+                : HealthStatusEnum.AVAILABLE,
+            checkedUrl: service.checkUrl,
+            name: service.name,
+        };
+    }
 
     public async createService(service: CreateInspectedServiceDto): Promise<InspectedServiceDbEntity> {
         const { raw: [result] } = await this.inspectedServiceRepository.createQueryBuilder()
