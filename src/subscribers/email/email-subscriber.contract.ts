37fd8bb18d441290c48755cd2520fad46a7fad8c
nik-kita <nik.moiseienko@gmail.com>
2022-02-09 11:12:29 +0200

    feat(email-subscriber): it works

--- src/subscribers/email/email-subscriber.contract.ts
+++ src/subscribers/email/email-subscriber.contract.ts
@@ -1,16 +1,7 @@
 import { TSubscriberContract } from '../all-subscribers-map';
-import { HealthReportResDto } from '../../general/health-report.res.dto';
 import { SubscriberTypeEnum } from '../subscriber-type.enum';
 import { EmailSubscriberDto } from './emai-subscriber.dto';
-import { EmailSubscriber } from './email.subscriber';
-
-let counter = 0;
-
-function emailOnMessage(message: HealthReportResDto) {
-    console.log('=== EMAIL ===');
-    console.log(message);
-    console.log('-'.repeat(Math.floor(Math.random() * 100)), ++counter);
-}
+import { EmailSubscriber, emailSubscriberOnMessage } from './email.subscriber';
 
 export const EMAIL_SUBSCRIBER_CONTRACT: TSubscriberContract<SubscriberTypeEnum.EMAIL> = {
     type: SubscriberTypeEnum.EMAIL,
@@ -20,6 +11,6 @@ export const EMAIL_SUBSCRIBER_CONTRACT: TSubscriberContract<SubscriberTypeEnum.E
         return true;
     },
     async resolveSubscriber(dto: EmailSubscriberDto) {
-        return EmailSubscriber.get(dto.type, emailOnMessage);
+        return EmailSubscriber.get(dto.type, emailSubscriberOnMessage);
     },
 };
--- src/subscribers/email/email.subscriber.ts
+++ src/subscribers/email/email.subscriber.ts
@@ -1,8 +1,40 @@
+import { createTransport, Transporter, TransportOptions } from 'nodemailer';
 import { BaseSubscriber, TOnmessage } from '../../components/subscription/subscriber-declaration/base-subscriber';
+import { HealthReportResDto } from '../../general/health-report.res.dto';
 import { SubscriberTypeEnum } from '../subscriber-type.enum';
 
 export class EmailSubscriber extends BaseSubscriber<SubscriberTypeEnum.EMAIL> {
-    public static async get(type: SubscriberTypeEnum.EMAIL, onMessage: TOnmessage): Promise<EmailSubscriber> {
-        return Promise.resolve(new EmailSubscriber(type, onMessage));
+    public static transporter: Transporter;
+
+    protected constructor(
+        type: SubscriberTypeEnum.EMAIL,
+        onMessage: TOnmessage,
+    ) {
+        super(type, onMessage);
     }
+
+    public static async get(type: SubscriberTypeEnum.EMAIL, onMessage: TOnmessage): Promise<EmailSubscriber> {
+        console.info(process.env.EMAIL_USER);
+
+        EmailSubscriber.transporter = await createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASSWORD,
+            },
+        } as TransportOptions);
+
+        return new EmailSubscriber(type, onMessage);
+    }
+}
+
+export async function emailSubscriberOnMessage(this: EmailSubscriber, message: HealthReportResDto) {
+    // eslint-disable-next-line no-use-before-define
+    EmailSubscriber.transporter.sendMail({
+        from: process.env.EMAIL_USER,
+        to: 'volsknik@gmail.com',
+        subject: message.name,
+        text: message.status,
+        html: `<h1>${message.status}</h1><hr>${message.checkedUrl}`,
+    });
 }
