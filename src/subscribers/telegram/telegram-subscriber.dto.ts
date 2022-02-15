03b7f4745d9e7beff66dfeadf4d28f3fb04bf11e
nik <nik.moiseienko@gmail.com>
2022-02-15 06:46:02 +0200

    feat(extracustmchecck decorator): add possibility to make extra async validation before subscriber

--- src/subscribers/telegram/telegram-subscriber.dto.ts
+++ src/subscribers/telegram/telegram-subscriber.dto.ts
@@ -1,50 +1,37 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, Matches, registerDecorator } from 'class-validator';
+import { IsNotEmpty, Matches } from 'class-validator';
 import { Telegram } from 'telegraf';
 import { RegisterSubscriberDto } from '../../components/subscription/dto/register-subscriber.dto';
+import ExtraCustomCheck from '../../general/extra-custom-check.decorator';
 import { SubscriberTypeEnum } from '../subscriber-type.enum';
 import { TelegramSubscriber } from './telegram.subscriber';
 
-export default function CheckMember() {
-    return (object: any, propertyName: string) => {
-        registerDecorator({
-            name: 'Exist',
-            target: object.constructor,
-            propertyName,
-            validator: {
-                async validate(value: any) {
-                    if (!value) return true;
-                    if (!TelegramSubscriber.telegram) {
-                        TelegramSubscriber.telegram = new Telegram(process.env.TELEGRAM_API_KEY);
-                    }
+async function check(value: any) {
+    if (!TelegramSubscriber.telegram) {
+        // TODO find more protected way ot gurantii default API for demonstration, update real telegram api key
+        // or as minimum after production remove default value, reset token
+        TelegramSubscriber.telegram = new Telegram(process.env.TELEGRAM_API_KEY || '5169422062:AAFO0OH_Zj7ln2L-I0KoSTNTgD75t74Dno0');
+    }
 
-                    const updates = await TelegramSubscriber.telegram.getUpdates(1000000, 1000, 0, ['chat_member']) as {
-                        message: {
-                            from: {
-                                username: string,
-                                id: number,
-                            }
-                        }
-                    }[];
-                    const members = updates.map(({ message }) => ({
-                        username: message.from.username,
-                        id: message.from.id,
-                    }));
+    const updates = await TelegramSubscriber.telegram.getUpdates(1000000, 1000, 0, ['chat_member']) as {
+        message: {
+            from: {
+                username: string,
+                id: number,
+            }
+        }
+    }[];
+    const members = updates.map(({ message }) => ({
+        username: message.from.username,
+        id: message.from.id,
+    }));
 
-                    const member = members.find((m) => m.username === value);
+    const member = members.find((m) => m.username === value);
 
-                    if (member) TelegramSubscriber.members.push(member);
+    if (member) TelegramSubscriber.members.push(member);
 
-                    return !!member;
-                },
-            },
-            options: {
-                message: () => `Before all - open Telegram and send any message to ${process.env.TELEGRAM_BOT_NIKNAME}`,
-            },
-        });
-    };
+    return !!member;
 }
-
 export class TelegramSubscriberDto extends RegisterSubscriberDto {
     @ApiProperty({
         type: SubscriberTypeEnum.TELEGRAM,
@@ -56,6 +43,6 @@ export class TelegramSubscriberDto extends RegisterSubscriberDto {
         type: String,
     })
     @IsNotEmpty()
-    @CheckMember()
+    @ExtraCustomCheck(check, `Send any message in Telegram to ${process.env.TELEGRAM_BOT_NIKNAME || '@tontiHealthCheckerBot'} before`)
     username: string;
 }
